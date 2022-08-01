f4fd7b8435f815d5ffcef6d4ef6f3e35a2868964
Grey Altr <grey.sifr@gmail.com>
2022-07-31 21:22:00 -0700

    further squigly fix

--- candies-data.js
+++ candies-data.js
@@ -3,12 +3,12 @@ export const candies = [
         name: 'smarties',
         color: 'clear',
         owner: 'smarties candy co.'
-    }
+    },
     {
         name: 'starburst',
         color: 'yellow',
         owner: 'wrigley co.'
-    }
+    },
     {
         name: 'skittles',
         color: 'red',
