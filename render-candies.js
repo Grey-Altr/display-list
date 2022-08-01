6ddee5f7d5e6dafe587e748f833c95c1595ba34a
Grey Altr <grey.sifr@gmail.com>
2022-07-31 17:24:12 -0700

    created list in candies-data.js, and created element in render-candies.js

---
+++ render-candies.js
@@ -0,0 +1,7 @@
+import { candies } from './candies-data.js';
+
+const candiesListEl = document.getElementById('candies-list');
+
+for (let candy of candies){
+    const candiesEl = document.createElement('p');
+};
