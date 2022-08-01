2f10b84313c48016ca2614e9cfb4ece1525911c9
Grey Altr <grey.sifr@gmail.com>
2022-07-31 21:18:20 -0700

    instantiated export-imports to app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-
+import { candies } from './candies-data.js';
+import { renderCandyItem } from './render-candy.js';
 // initialize global state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
+
+
+const candiesListEl = document.getElementById('candies-list');
+
+for (let candy of candies) {
+  const candiesEl = renderCandyItem(candy);
+
+  candiesListEl.append(candiesEl);
+
+}
--- render-candy.js
+++ render-candy.js
@@ -1,15 +1,4 @@
-import { candies } from './candies-data.js';
-
-const candiesListEl = document.getElementById('candies-list');
-
-for (let candy of candies){
-    const candiesEl = renderCandyItem(candy);
-
-    candiesListEl.append(candiesEl);
-    
-}
-
-function renderCandyItem(pieceOfCandy){
+export function renderCandyItem(pieceOfCandy){
     const pTag = document.createElement('div', 'p');
     pTag.textContent = pieceOfCandy;
     pTag.classList.add('candies-list');
